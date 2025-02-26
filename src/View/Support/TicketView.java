ba4f4bd2b82319b9133383194b95710b0619e59a
Arctic904 <53384010+Arctic904@users.noreply.github.com>
2025-02-26 17:57:04 -0500

    update ticket view

--- src/View/Support/TicketView.java
+++ src/View/Support/TicketView.java
@@ -1,4 +1,31 @@
 package View.Support;
 
+import Controller.TicketController;
+import Model.Login.User;
+import Model.Support.Message;
+import Model.Support.SupportUser;
+import Model.Support.Ticket;
+
 public class TicketView {
+    private TicketController controller;
+    private Ticket ticket;
+
+    public TicketView(TicketController controller) {
+        this.controller = controller;
+    }
+
+    public void newTicket(String initialMessage, User user, SupportUser supportUser) {
+        Ticket ticket1 = new Ticket(user, supportUser);
+        ticket1.addMessage(new Message(initialMessage));
+        controller.addTicket(ticket1);
+        ticket = ticket1;
+    }
+
+    public Ticket getTicket() {
+        return ticket;
+    }
+
+    public void closeTicket() {
+        ticket = null;
+    }
 }
