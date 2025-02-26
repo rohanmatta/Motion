889cafe0185bfb6a9b9a834fff75272142bb8a44
syd18b <snb5728@psu.edu>
2025-02-26 17:13:20 -0500

    commit

--- src/Model/WarmupAndRecovery/Admin.java
+++ src/Model/WarmupAndRecovery/Admin.java
@@ -9,7 +9,49 @@ public class Admin {
         this.name = name;
     }
 
-    public String getAdminId() { return adminId; }
-    public String getName() { return name; }
 
+    public String getAdminId() {
+        return adminId;
+    }
+
+
+    public void setAdminId(String adminId) {
+        this.adminId = adminId;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void displayAdminDetails() {
+        System.out.println("Admin ID: " + adminId);
+        System.out.println("Name: " + name);
+    }
+
+
+    public boolean equals(Admin other) {
+        if (other == null) return false;
+        return this.adminId.equals(other.adminId) && this.name.equals(other.name);
+    }
+
+
+    public void updateName(String newName) {
+        this.name = newName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminId='" + adminId + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
