1906301d9963cbce15fd27d372b0344f1158657d
Rohan Matta <rohan.matta2004@gmail.com>
2025-02-26 16:54:05 -0500

    deleted sydney

--- src/WarmupAndRecovery/Controller/ClientAuth.java
+++
@@ -1,7 +0,0 @@
-package WarmupAndRecovery.Controller;
-
-public class ClientAuth {
-    public boolean login(String username, String password) {
-        return username.equals("client") && password.equals("securePass");
-    }
-}
--- src/WarmupAndRecovery/Controller/TrainerAuth.java
+++
@@ -1,7 +0,0 @@
-package WarmupAndRecovery.Controller;
-
-public class TrainerAuth {
-    public boolean login(String username, String password) {
-        return username.equals("trainer") && password.equals("securePass");
-    }
-}
