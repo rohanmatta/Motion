a4726c55051d4758b8d57976fcc15b796246c495
Brett Cunningham <brettc340@gmail.com>
2025-04-10 19:59:07 -0400

    design pattern

---
+++ src/Model/TrackProgress/Observer.java
@@ -0,0 +1,7 @@
+package Model.TrackProgress;
+
+public interface Observer {
+    void update(ProgressData progressData);
+}
+
+
---
+++ src/Model/TrackProgress/Subject.java
@@ -0,0 +1,9 @@
+package Model.TrackProgress;
+
+public interface Subject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
+}
+
+
