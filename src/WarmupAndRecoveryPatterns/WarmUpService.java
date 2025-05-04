df71949f1ef2116b341736a45eabec0d86a44f36
syd18b <snb5728@psu.edu>
2025-05-04 16:06:31 -0400

    push

--- src/WarmupAndRecoveryPatterns/WarmUpService.java
+++ src/WarmupAndRecoveryPatterns/WarmUpService.java
@@ -1,12 +1,17 @@
 package WarmupAndRecoveryPatterns;
 
+import Model.UserHistoryManager;
+import java.time.LocalDate;
+
 public class WarmUpService {
 
-    public void startWarmUp(String warmupType) {
+    public void startWarmUp(String warmupType, String username) {
         System.out.println("Starting " + warmupType + " Warmup...");
+        UserHistoryManager.log(username, "warmup", warmupType + " routine started", LocalDate.now().toString());
     }
 
-    public void logWorkout(String workoutDetails) {
+    public void logWorkout(String workoutDetails, String username) {
         System.out.println("Logging workout: " + workoutDetails);
+        UserHistoryManager.log(username, "warmup", workoutDetails, LocalDate.now().toString());
     }
 }
